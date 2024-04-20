6b9b527760529a5c9ab621181af68522786cee55
Anton Gladyshev <returnstrike@yandex.com>
2024-04-21 02:51:21 +0300

    added more cards

--- phrases.js
+++ phrases.js
@@ -7,6 +7,31 @@ cards = [
    "gr_f": "Από πού είσαε;",
    "gr_q": "Από ... είσαε;",
    "gr_a": "πού"},
+  {"en_t": "I am from France",
+   "gr_f": "Eίμαι από τη Γαλλία",
+   "gr_q": "... (I am) από τη Γαλλία",
+   "gr_a": "Eίμαι",
+   "arg1": ["τη Γαλλία","τη Ρωσία","την Κυπρο"]},
+  {"en_t": "From which part?",
+   "gr_f": "Από ποιο μέρος;",
+   "gr_q": "Από ποιο ...(part);",
+   "gr_a": "μέρος"},
+  {"en_t": "From which part?",
+   "gr_f": "Από ποιο μέρος;",
+   "gr_q": "Από ...(which) μέρος;",
+   "gr_a": "ποιο"},
+  {"en_t": "Where are you from?",
+   "gr_f": "Εσείς από πού είστε;",
+   "gr_q": "...(you, f) από πού είστε;",
+   "gr_a": "Εσείς"},
+  {"en_t": "Where are you from?",
+   "gr_f": "Εσύ από πού είσαι;",
+   "gr_q": "... (you, if) από πού είσαι;",
+   "gr_a": "Εσύ"},
+  {"en_t": "Where are you from?",
+   "gr_f": "Εσείς από πού είστε;",
+   "gr_q": "... (you, f) από πού είστε;",
+   "gr_a": "Εσείς"}
 ]
 let current_card = 0
 function nextCard(){
@@ -21,13 +46,13 @@ function nextCard(){
 }
 nextCard()
 function showCorrectAnswer() {
-  const curQuestionAnswer = cards[current_card].gr_a;
-  document.getElementById("divCorrectAnswer").innerHTML=curQuestionAnswer;
+  const curQuestionAnswer = cards[current_card].gr_a.toLowerCase();
+  document.getElementById("divCorrectAnswer").innerHTML=curQuestionAnswer.toLowerCase();
   document.getElementById("divCorrectAnswer").style.visibility = "visible";
 }
 function check() {
-  const userAnswer        = document.getElementById("user_response").value;
-  const curQuestionAnswer = cards[current_card].gr_a;
+  const userAnswer        = document.getElementById("user_response").value.toLowerCase();;
+  const curQuestionAnswer = cards[current_card].gr_a.toLowerCase();
   if (userAnswer === curQuestionAnswer) {
     document.getElementById("divCheckResult").innerHTML="CORRECT";
   } else {
