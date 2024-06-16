feb18d97ee56b97d064d4efbf1b0ad74fa4547a9
Anton Gladyshev <returnstrike@yandex.com>
2024-06-16 12:18:06 +0300

    changed structure and stuff

---
+++ cards_places.js
@@ -0,0 +1,56 @@
+cards = [
+  {"gr": "το σπίτι",
+   "en": "the house",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Το σπίτι μου"]},
+   ],
+   "en_ex": [
+     {"present":["My house"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/house.jpg"},
+  {"gr": "ΤΜΗΜΑ ΟΔΙΚΩΝ ΜΕΤΑΦΟΡΩΝ (TOM)",
+   "en": "Road Transport Department",
+   "level": 1,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Εγο είμαι στο ΤΜΗΜΑ ΟΔΙΚΩΝ ΜΕΤΑΦΟΡΩΝ (TOM)"]},
+   ],
+   "en_ex": [
+     {"present":["I am at Road Transport Department"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/house.jpg"},
+  {"gr": "το μουσείο",
+   "en": "the museum",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Εγο είμαι στο μουσείο τώρα"]},
+   ],
+   "en_ex": [
+     {"present":["I am at the museum now"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/house.jpg"},
+  {"gr": "η στάση",
+   "en": "the stop",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Είναι η στάση (του λεωφορείου)"]},
+   ],
+   "en_ex": [
+     {"present":["It's the (bus) stop"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/house.jpg"},
+
+ ]
+nextCard();
