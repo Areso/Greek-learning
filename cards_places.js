b338c97d96029cac9dd59a2a2adb70398c951833
Anton Gladyshev <returnstrike@yandex.com>
2024-06-16 15:01:09 +0300

    added a few words

--- cards_places.js
+++ cards_places.js
@@ -24,7 +24,7 @@ cards = [
    ],
    "ctable": [
    ],
-   "img":"decks/house.jpg"},
+   "img":"decks/tom.jpg"},
   {"gr": "το μουσείο",
    "en": "the museum",
    "level": 0,
@@ -37,7 +37,7 @@ cards = [
    ],
    "ctable": [
    ],
-   "img":"decks/house.jpg"},
+   "img":"decks/museum.jpg"},
   {"gr": "η στάση",
    "en": "the stop",
    "level": 0,
@@ -50,7 +50,49 @@ cards = [
    ],
    "ctable": [
    ],
-   "img":"decks/house.jpg"},
-
+   "img":"decks/stop.jpg"},
+  {"gr": "το (γενικό) νοσοκομείο",
+   "en": "the (general) hospital",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Εγο είμαι στο (γενικό) νοσοκομείο",
+                 "Είμαι κοντά στο (παλιό) νοσοκομείο"
+     ]},
+   ],
+   "en_ex": [
+     {"present":["I am at the (general) hospital",
+                 "I am near the (old) hospital"
+     ]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/hospital.jpg"},
+  {"gr": "το σχολείο",
+   "en": "the school",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Εγο είμαι στο σχολείο"]},
+   ],
+   "en_ex": [
+     {"present":["I'm at school"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/school.jpg"},
+  {"gr": "το ξενοδοχείο",
+   "en": "the hotel",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Μένω στο ξενοδοχείο"]},
+   ],
+   "en_ex": [
+     {"present":["I live in the hotel"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/hotel.jpg"},
  ]
 nextCard();
