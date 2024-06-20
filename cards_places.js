266d6a6775120af43d2f7c0560ab500c44870eaf
Anton Gladyshev <returnstrike@yandex.com>
2024-06-20 22:00:14 +0300

    added a few more locations

--- cards_places.js
+++ cards_places.js
@@ -94,5 +94,96 @@ cards = [
    "ctable": [
    ],
    "img":"decks/hotel.jpg"},
+  {"gr": "ο δρόμος",
+   "en": "the street",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Είναι ο δρόμος προς την παλιά πόλη"]},
+   ],
+   "en_ex": [
+     {"present":["It is the street to the old town"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/street.jpg"},
+  {"gr": "η πλατεία",
+   "en": "the square",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Είναι η πλατεία της παλιάς πόλης"]},
+   ],
+   "en_ex": [
+     {"present":["It is the street to the old town"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/square.jpg"},
+  {"gr": "το καφενείο",
+   "en": "the cafe",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Υπάρχει καλός κυπριακός καφές στο καφέ"]},
+   ],
+   "en_ex": [
+     {"present":["There is good Cyprus coffee in this cafe"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/cafe.jpg"},
+  {"gr": "το φαρμακείο",
+   "en": "the pharmacy",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Υπάρχει φαρμακείο;"]},
+   ],
+   "en_ex": [
+     {"present":["Is there a pharmacy?"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/pharmacy.jpg"},
+  {"gr": "το περίπτερο",
+   "en": "the convenient store (minimarket)",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Πού είναι ένα περίπτερο;"]},
+   ],
+   "en_ex": [
+     {"present":["Where is a convenient store (minimarket)?"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/minimarket.jpg"},
+   {"gr": "το kiosk",
+    "en": "the convenient store (minimarket)",
+    "level": 0,
+    "constructions": ["present"],
+    "gr_ex": [
+      {"present":["Πού είναι ένα kiosk;"]},
+    ],
+    "en_ex": [
+      {"present":["Where is a convenient store (minimarket)?"]},
+    ],
+    "ctable": [
+    ],
+    "img":"decks/minimarket.jpg"},
+    {"gr": "το μίνι μάρκετ",
+      "en": "the convenient store (minimarket)",
+      "level": 0,
+      "constructions": ["present"],
+      "gr_ex": [
+        {"present":["Πού είναι ένα μίνι μάρκετ;"]},
+      ],
+      "en_ex": [
+        {"present":["Where is a convenient store (minimarket)?"]},
+      ],
+      "ctable": [
+      ],
+      "img":"decks/minimarket.jpg"},
  ]
 nextCard();
