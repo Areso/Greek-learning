db687eeb5d2e38d3ffc28acb461c285a8adfce1f
Anton Gladyshev <returnstrike@yandex.com>
2024-06-24 17:26:17 +0300

    added colors A0

---
+++ cards_colors.js
@@ -0,0 +1,239 @@
+cards = [
+  {"gr": "το μαυρο",
+   "en": "the black",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["το μαύρο σπίτι",
+                 "το μαύρο μάθημα",
+                 "το μαύρο θέατρο",
+                 "το μαύρο σκυλί",
+                 "ο μαύρος δρόμος",
+                 "ο μαύρος",
+                 "ο μαύρος πατέρας",
+                 "ο μαύρος θεός",
+                 "η μαύρη γυναίκα",
+                 "η μαύρη Ελένη",
+                 "η μαύρη μητέρα",
+                 "η μαύρη θεά"
+     ]},
+   ],
+   "en_ex": [
+     {"present":["the black home",
+                 "the black lesson",
+                 "the black theater",
+                 "the black dog",
+                 "the black road",
+                 "the black man",
+                 "the black father",
+                 "the black god",
+                 "the black woman",
+                 "the black Helen",
+                 "the black mother",
+                 "the black goddess"
+     ]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/black.jpg"},
+  {"gr": "το λευκό / το άσπρος",
+   "en": "the white",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+    {"present":["το λευκό / άσπρο σπίτι",
+                "το λευκό / άσπρο μάθημα",
+                "το λευκό / άσπρο θέατρο",
+                "το λευκό / άσπρο κυλί",
+                "ο λευκός / άσπρος δρόμος",
+                "ο λευκός",
+                "ο λευκός / άσπρος πατέρας",
+                "ο λευκός / άσπρος θεός",
+                "η λευκή / άσπρή γυναίκα",
+                "η λευκή / άσπρή Ελένη",
+                "η λευκή / άσπρή μητέρα",
+                "η λευκή / άσπρή θεά",
+                "το λευκό κρασί"
+    ]},
+   ],
+   "en_ex": [
+    {"present":["the white home",
+                "the white lesson",
+                "the white theater",
+                "the white dog",
+                "the white road",
+                "the white man",
+                "the white father",
+                "the white god",
+                "the white woman",
+                "the white Helen",
+                "the white mother",
+                "the white goddess",
+                "the white wine"
+    ]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/tom.jpg"},
+  {"gr": "το μουσείο",
+   "en": "the museum",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Εγο είμαι στο μουσείο τώρα"]},
+   ],
+   "en_ex": [
+     {"present":["I am at the museum now"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/museum.jpg"},
+  {"gr": "η στάση",
+   "en": "the stop",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Είναι η στάση (του λεωφορείου)"]},
+   ],
+   "en_ex": [
+     {"present":["It's the (bus) stop"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/stop.jpg"},
+  {"gr": "το (γενικό) νοσοκομείο",
+   "en": "the (general) hospital",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Εγο είμαι στο (γενικό) νοσοκομείο",
+                 "Είμαι κοντά στο (παλιό) νοσοκομείο"
+     ]},
+   ],
+   "en_ex": [
+     {"present":["I am at the (general) hospital",
+                 "I am near the (old) hospital"
+     ]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/hospital.jpg"},
+  {"gr": "το σχολείο",
+   "en": "the school",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Εγο είμαι στο σχολείο"]},
+   ],
+   "en_ex": [
+     {"present":["I'm at school"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/school.jpg"},
+  {"gr": "το ξενοδοχείο",
+   "en": "the hotel",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Μένω στο ξενοδοχείο"]},
+   ],
+   "en_ex": [
+     {"present":["I live in the hotel"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/hotel.jpg"},
+  {"gr": "ο δρόμος",
+   "en": "the street",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Είναι ο δρόμος προς την παλιά πόλη"]},
+   ],
+   "en_ex": [
+     {"present":["It is the street to the old town"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/street.jpg"},
+  {"gr": "η πλατεία",
+   "en": "the square",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Είναι η πλατεία της παλιάς πόλης"]},
+   ],
+   "en_ex": [
+     {"present":["It is the street to the old town"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/square.jpg"},
+  {"gr": "το καφενείο",
+   "en": "the cafe",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Υπάρχει καλός κυπριακός καφές στο καφέ"]},
+   ],
+   "en_ex": [
+     {"present":["There is good Cyprus coffee in this cafe"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/cafe.jpg"},
+  {"gr": "το φαρμακείο",
+   "en": "the pharmacy",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Υπάρχει φαρμακείο;"]},
+   ],
+   "en_ex": [
+     {"present":["Is there a pharmacy?"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/pharmacy.jpg"},
+  {"gr": "το περίπτερο",
+   "en": "the convenient store (minimarket)",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["Πού είναι ένα περίπτερο;"]},
+   ],
+   "en_ex": [
+     {"present":["Where is a convenient store (minimarket)?"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/minimarket.jpg"},
+   {"gr": "το kiosk",
+    "en": "the convenient store (minimarket)",
+    "level": 0,
+    "constructions": ["present"],
+    "gr_ex": [
+      {"present":["Πού είναι ένα kiosk;"]},
+    ],
+    "en_ex": [
+      {"present":["Where is a convenient store (minimarket)?"]},
+    ],
+    "ctable": [
+    ],
+    "img":"decks/minimarket.jpg"},
+    {"gr": "το μίνι μάρκετ",
+      "en": "the convenient store (minimarket)",
+      "level": 0,
+      "constructions": ["present"],
+      "gr_ex": [
+        {"present":["Πού είναι ένα μίνι μάρκετ;"]},
+      ],
+      "en_ex": [
+        {"present":["Where is a convenient store (minimarket)?"]},
+      ],
+      "ctable": [
+      ],
+      "img":"decks/minimarket.jpg"},
+ ]
+nextCard();
