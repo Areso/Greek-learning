8506eb4baf84b0329ce6f4bffc49191d243373a2
Anton Gladyshev <returnstrike@yandex.com>
2024-12-22 14:05:52 +0200

    verbs A1 done: present, simple past

---
+++ verbs_to_learn.js
@@ -0,0 +1,349 @@
+cards = [
+ {"gr": "είμαι",
+  "en": "to be",
+  "constructions": ["present","αόριστος"],
+  "gr_ex": [
+    {"present":["Εγώ είμαι ένας άντρας"]},
+    {"αόριστος":["Εγώ ήμουν ένας άντρας"]},
+  ],
+  "en_ex": [
+    {"present":["I am a man"]},
+    {"αόριστος":["I was a man"]},
+  ],
+  "ctable": [
+    {"present":["είμαι","είσαι","είναι",
+      "είμαστε","είσαστε/είστε","είναι"]},
+    {"αόριστος":["ήμουν","ήσουν","ήταν",
+      "ήμασταν/ήμαστε","ήσασταν/ήσαστε","ήταν"]},
+    ],
+  "img":"decks/big_imgs/arrow.jpg",
+  "labels":["irregular"]},
+
+  {"gr": "φτάνω",
+   "en": "to arrive/to achive",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ φτάνω τώρα"]},
+     {"αόριστος":["εγώ έφτασα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am arriving now"]},
+     {"αόριστος":["I arrived yesterday"]},
+   ],
+   "ctable": [
+     {"present":["φτάνω τώρα","φτάνεις τώρα","φτάνει τώρα",
+       "φτάνουμε τώρα","φτάνετε τώρα","φτάνουν τώρα"]},
+     {"αόριστος":["έφτασα χτες","έφτασες χτες","έφτασε χτες",
+       "φτάσαμε χτες","φτάσατε χτες","έφτασαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -νω -> -σα"]},
+
+  {"gr": "λείπω",
+   "en": "to leave",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ λείπω τώρα"]},
+     {"αόριστος":["εγώ έλειψα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am leaving now"]},
+     {"αόριστος":["I left yesterday"]},
+   ],
+   "ctable": [
+     {"present":["λείπω τώρα","λείπεις τώρα","λείπει τώρα",
+       "λείπουμε τώρα","λείπετε τώρα","λείπουν τώρα"]},
+     {"αόριστος":["έλειψα χτες","έλειψες χτες","έλειψε χτες",
+       "λείψαμε χτες","λείψατε χτες","έλειψαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","-πω -> -ψα"]},
+
+  {"gr": "ανάβω",
+   "en": "to turn on / to light on",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ ανάβω τώρα"]},
+     {"αόριστος":["εγώ άναψα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am turning on now"]},
+     {"αόριστος":["I turned on yesterday"]},
+   ],
+   "ctable": [
+     {"present":["ανάβω τώρα","ανάβεις τώρα","ανάβει τώρα",
+       "ανάβουμε τώρα","ανάβετε τώρα","ανάβουν τώρα"]},
+     {"αόριστος":["άναψα χτες","άναψες χτες","άναψε χτες",
+       "ανάψαμε χτες","ανάψατε χτες","άναψαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -βω -> -ψα"]},
+
+  {"gr": "γράφω",
+   "en": "to write",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ γράφω τώρα"]},
+     {"αόριστος":["εγώ έγραψα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am writing now"]},
+     {"αόριστος":["I wrote yesterday"]},
+   ],
+   "ctable": [
+     {"present":["γράφω τώρα","γράφεις τώρα","γράφει τώρα",
+       "γράφουμε τώρα","γράφετε τώρα","γράφουν τώρα"]},
+     {"αόριστος":["έγραψα χτες","έγραψες χτες","έγραψε χτες",
+       "γράψαμε χτες","γράψατε χτες","έγραψαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -βω -> -ψα"]},
+
+  {"gr": "αγοράζω",
+   "en": "to buy",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ αγοράζω τώρα"]},
+     {"αόριστος":["εγώ αγόρασα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am buying now"]},
+     {"αόριστος":["I bought yesterday"]},
+   ],
+   "ctable": [
+     {"present":["αγοράζω τώρα","αγοράζεις τώρα","αγοράζει τώρα",
+       "αγοράζουμε τώρα","αγοράζετε τώρα","αγοράζουν(ε) τώρα"]},
+     {"αόριστος":["αγόρασα χτες","αγόρασες χτες","αγόρασε χτες",
+       "αγοράσαμε χτες","αγοράσατε χτες","αγόρασαν/αγοράσανε χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -ζω -> -σα"]},
+
+  {"gr": "αλλάζω",
+   "en": "to change",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ το αλλάζω τώρα"]},
+     {"αόριστος":["εγώ το αλλάξα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am changing it now"]},
+     {"αόριστος":["I changed it yesterday"]},
+   ],
+   "ctable": [
+     {"present":["αλλάζω τώρα","αλλάζεις τώρα","αλλάζει τώρα",
+       "αλλάζουμε τώρα","αλλάζετε τώρα","αλλάζουν τώρα"]},
+     {"αόριστος":["άλλαξα χτες","άλλαξες χτες","άλλαξε χτες",
+       "αλλάξαμε χτες","αλλάξατε χτες","άλλαξαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος (exc): -ζω -> -ξα"]},
+
+  {"gr": "ανοίγω",
+   "en": "to open",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ το ανοίγω τώρα"]},
+     {"αόριστος":["εγώ το άνοιξα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am opening it now"]},
+     {"αόριστος":["I opened it yesterday"]},
+   ],
+   "ctable": [
+     {"present":["ανοίγω τώρα","ανοίγεις τώρα","ανοίγει τώρα",
+       "ανοίγουμε τώρα","ανοίγετε τώρα","ανοίγουν τώρα"]},
+     {"αόριστος":["άνοιξα χτες","άνοιξες χτες","άνοιξε χτες",
+       "ανοίξαμε χτες","ανοίξατε χτες","άνοιξαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -γω/χω/χνω -> -ξα"]},
+
+  {"gr": "δουλεύω",
+   "en": "to work",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ δουλεύω τώρα"]},
+     {"αόριστος":["εγώ δούλεψα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am working now"]},
+     {"αόριστος":["I worked yesterday"]},
+   ],
+   "ctable": [
+     {"present":["δουλεύω τώρα","δουλεύεις τώρα","δουλεύει τώρα",
+       "δουλεύουμε τώρα","δουλεύετε τώρα","δουλεύουν τώρα"]},
+     {"αόριστος":["δούλεψα χτες","δούλεψες χτες","δούλεψε χτες",
+       "δουλέψαμε χτες","δουλέψατε χτες","δούλεψαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -εύω/πω/φω/βω -> -ψα"]},
+
+  {"gr": "παίζω",
+   "en": "to play",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ παίζω τώρα"]},
+     {"αόριστος":["εγώ έπαιξα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am playing now"]},
+     {"αόριστος":["I played yesterday"]},
+   ],
+   "ctable": [
+     {"present":["παίζω τώρα","παίζεις τώρα","παίζει τώρα",
+       "παίζουμε τώρα","παίζετε τώρα","παίζουν τώρα"]},
+     {"αόριστος":["έπαιξα χτες","έπαιξες χτες","έπαιξε χτες",
+       "παίξαμε χτες","παίξατε χτες","έπαιξαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος (exc): -ζω -> -ξα"]},
+
+  {"gr": "πειράζω",
+   "en": "to tease, to trial",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ πειράζω τώρα"]},
+     {"αόριστος":["εγώ πείραξα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am teasing now"]},
+     {"αόριστος":["I teased yesterday"]},
+   ],
+   "ctable": [
+     {"present":["πειράζω τώρα","πειράζεις τώρα","πειράζει τώρα",
+       "πειράζουμε τώρα","πειράζετε τώρα","πειράζουν τώρα"]},
+     {"αόριστος":["πείραξα χτες","πείραξες χτες","πείραξε χτες",
+       "πειράξαμε χτες","πειράξατε χτες","πείραξαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος (exc): -ζω -> -ξα"]},
+
+  {"gr": "πληρώνω",
+   "en": "to pay",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ πληρώνω τώρα"]},
+     {"αόριστος":["εγώ πλήρωσα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am paying now"]},
+     {"αόριστος":["I payed yesterday"]},
+   ],
+   "ctable": [
+     {"present":["πληρώνω τώρα","πληρώνεις τώρα","πληρώνει τώρα",
+       "πληρώνουμε τώρα","πληρώνετε τώρα","πληρώνουν(ε) τώρα"]},
+     {"αόριστος":["πλήρωσα χτες","πλήρωσες χτες","πλήρωσε χτες",
+       "πληρώσαμε χτες","πληρώσατε χτες","πλήρωσαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -νω/ζω -> -σα"]},
+
+  
+  {"gr": "προσέχω",
+   "en": "to pay attention/to be careful",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ προσέχω τώρα"]},
+     {"αόριστος":["εγώ πρόσεξα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am in attention now"]},
+     {"αόριστος":["I was in attention yesterday"]},
+   ],
+   "ctable": [
+     {"present":["προσέχω τώρα","προσέχεις τώρα","προσέχει τώρα",
+       "προσέχουμε τώρα","προσέχετε τώρα","προσέχουν τώρα"]},
+     {"αόριστος":["πρόσεξα χτες","πρόσεξες χτες","πρόσεξε χτες",
+       "προσέξαμε χτες","προσέξατε χτες","πρόσεξαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -γω/χω/χνω -> -ξα"]},
+
+
+  {"gr": "φτιάχνω",
+   "en": "to create/do/fix",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ φτιάχνω τώρα"]},
+     {"αόριστος":["εγώ έφτιαξα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am creating now"]},
+     {"αόριστος":["I created yesterday"]},
+   ],
+   "ctable": [
+     {"present":["φτιάχνω τώρα","φτιάχνεις τώρα","φτιάχνει τώρα",
+       "φτιάχνουμε τώρα","φτιάχνετε τώρα","φτιάχνουν τώρα"]},
+     {"αόριστος":["έφτιαξα χτες","έφτιαξες χτες","έφτιαξε χτες",
+       "φτιάξαμε χτες","φτιάξατε χτες","έφτιαξαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος: -γω/χω/χνω -> -ψα"]},
+
+
+  {"gr": "φωνάζω",
+   "en": "to shout",
+   "constructions": ["present","αόριστος"],
+   "gr_ex": [
+     {"present":["εγώ φωνάζω τώρα"]},
+     {"αόριστος":["εγώ φώναξα χτες"]},
+   ],
+   "en_ex": [
+     {"present":["I am shouting now"]},
+     {"αόριστος":["I shouted yesterday"]},
+   ],
+   "ctable": [
+     {"present":["φωνάζω τώρα","φωνάζεις τώρα","φωνάζει τώρα",
+       "φωνάζουμε τώρα","φωνάζετε τώρα","φωνάζουν(ε) τώρα"]},
+     {"αόριστος":["φώναξα χτες","φώναξες χτες","φώναξε χτες",
+       "φωνάξαμε χτες","φωνάξατε χτες","φώναξαν χτες"]},
+   ],
+   "img":"decks/big_imgs/arrow.jpg",
+   "labels":["A1","αόριστος (exc): -ζω -> -ξα"]},
+
+]
+let current_card = 0
+personas = [
+    "Εγώ",
+    "Εσύ",
+    "Αυτός/αυτή/αυτό(m/f/it)",
+    "Εμείς",
+    "Εσείς(formal/plural)",
+    "Αυτοί/Αυτές/Αυτά",
+]
+function nextCard(){
+  const lenCards = cards.length
+  const card_number_to_ask = Math.floor(Math.random() * lenCards);
+  current_card   = card_number_to_ask;
+  console.log("card_number_to_ask",card_number_to_ask);
+  const lenConstructs= cards[card_number_to_ask].constructions.length;
+  const constrId     = Math.floor(Math.random() * lenConstructs);
+  console.log("constrId",constrId);
+  const constrName   = cards[card_number_to_ask].constructions[constrId];
+  console.log("constrName",constrName);
+  const lenExamples  = cards[card_number_to_ask].en_ex[constrId][constrName].length;
+  const exampledId   = Math.floor(Math.random() * lenExamples);
+  let   theText  = cards[card_number_to_ask].gr+"<br><br>";
+  theText       += cards[card_number_to_ask].en+"<br><br>";
+  theText       += cards[card_number_to_ask].gr_ex[constrId][constrName][exampledId]+"<br>";
+  theText       += cards[card_number_to_ask].en_ex[constrId][constrName][exampledId]+"<br><br>";
+  lenConj        = cards[card_number_to_ask].ctable[constrId][constrName].length;
+  if ("labels" in cards[card_number_to_ask]) {
+    theText     += cards[card_number_to_ask].labels+"<br><br>";
+  } else {
+    theText     += "<br><br>";
+  }
+  conjPos        = 0;
+  console.log(lenConj)
+  if (lenConj !== 0) {
+    console.log("check conj table is not blank")
+    while (conjPos<lenConj){
+      console.log("populate conj table")
+      theText     += personas[conjPos]+" "+ cards[card_number_to_ask].ctable[constrId][constrName][conjPos]+"<br>";
+      conjPos     += 1;
+    }
+  }
+  document.getElementById("div_question").innerHTML=theText;
+}
+nextCard();
