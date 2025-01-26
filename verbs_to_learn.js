d90d06e1b0055bf485a80a3caba4a45b880abdf1
Anton Gladyshev <returnstrike@yandex.com>
2025-01-26 19:10:10 +0200

    added train mode

--- verbs_to_learn.js
+++ verbs_to_learn.js
@@ -1743,37 +1743,75 @@ personas = [
     "Αυτοί/Αυτές/Αυτά",
 ]
 function nextCard(){
-  const lenCards = cards.length
+  const lenCards           = cards.length;
   const card_number_to_ask = Math.floor(Math.random() * lenCards);
-  current_card   = card_number_to_ask;
-  console.log("card_number_to_ask",card_number_to_ask);
-  const lenConstructs= cards[card_number_to_ask].constructions.length;
-  const constrId     = Math.floor(Math.random() * lenConstructs);
+  current_card             = card_number_to_ask;
+  console.log("card_number_to_ask",card_number_to_ask); 
+  const lenConstructs      = cards[card_number_to_ask].constructions.length;
+  const constrId           = Math.floor(Math.random() * lenConstructs);
   console.log("constrId",constrId);
-  const constrName   = cards[card_number_to_ask].constructions[constrId];
+  const constrName         = cards[card_number_to_ask].constructions[constrId]; 
   console.log("constrName",constrName);
-  const lenExamples  = cards[card_number_to_ask].en_ex[constrId][constrName].length;
-  const exampledId   = Math.floor(Math.random() * lenExamples);
-  let   theText  = cards[card_number_to_ask].gr+"<br><br>";
-  theText       += cards[card_number_to_ask].en+"<br><br>";
-  theText       += cards[card_number_to_ask].gr_ex[constrId][constrName][exampledId]+"<br>";
-  theText       += cards[card_number_to_ask].en_ex[constrId][constrName][exampledId]+"<br><br>";
-  lenConj        = cards[card_number_to_ask].ctable[constrId][constrName].length;
-  if ("labels" in cards[card_number_to_ask]) {
-    theText     += cards[card_number_to_ask].labels+"<br><br>";
-  } else {
-    theText     += "<br><br>";
-  }
-  conjPos        = 0;
-  console.log(lenConj)
-  if (lenConj !== 0) {
-    console.log("check conj table is not blank")
-    while (conjPos<lenConj){
-      console.log("populate conj table")
-      theText     += personas[conjPos]+" "+ cards[card_number_to_ask].ctable[constrId][constrName][conjPos]+"<br>";
-      conjPos     += 1;
+  const lenExamples        = cards[card_number_to_ask].en_ex[constrId][constrName].length;
+  const exampledId         = Math.floor(Math.random() * lenExamples);
+    
+  if (type==="memorize"){
+    let   theText  = cards[card_number_to_ask].gr+"<br><br>";
+    theText       += cards[card_number_to_ask].en+"<br><br>";
+    theText       += cards[card_number_to_ask].gr_ex[constrId][constrName][exampledId]+"<br>";
+    theText       += cards[card_number_to_ask].en_ex[constrId][constrName][exampledId]+"<br><br>";
+    lenConj        = cards[card_number_to_ask].ctable[constrId][constrName].length;
+    if ("labels" in cards[card_number_to_ask]) {
+      theText     += cards[card_number_to_ask].labels+"<br><br>";
+    } else {
+      theText     += "<br><br>";
     }
+    conjPos        = 0;
+    console.log(lenConj)
+    if (lenConj !== 0) {
+      console.log("check conj table is not blank")
+      while (conjPos<lenConj){
+        console.log("populate conj table")
+        theText     += personas[conjPos]+" "+ cards[card_number_to_ask].ctable[constrId][constrName][conjPos]+"<br>";
+        conjPos     += 1;
+      }
+    }
+    document.getElementById("div_question").innerHTML=theText;
   }
-  document.getElementById("div_question").innerHTML=theText;
+  if (type==="practice") {
+    document.getElementById("result_or_correct_answer").innerHTML="";
+    lenConj        = cards[card_number_to_ask].ctable[constrId][constrName].length;
+    if (lenConj === 0) {
+        console.log("next card, this one is not conjugated")
+        nextCard();        
+    }
+    if (lenConj !== 0) {
+        conjPos = Math.floor(Math.random() * lenConj);
+    }
+    conjugated    = cards[card_number_to_ask].ctable[constrId][constrName][conjPos];
+    question      = personas[conjPos]+" "+conjugated+" <br>";
+    correctAnswer = question.split(' ')[1] // first we have pronoun, then verb, then everything else
+    questionToAsk     = conjugated.split(' ');
+    questionToAsk[0]  = '----';
+    engVerb = " ("+ cards[card_number_to_ask].en + ")";
+    upd_questionToAsk = personas[conjPos]+" "+questionToAsk.join(' ')+engVerb;
+    htmlQuestion      = document.getElementById("div_question")
+    htmlQuestion.innerHTML = upd_questionToAsk
+  }
+}
+function check(){
+  userInput = document.getElementById("answer").value.trim().toLowerCase();
+  correctAnswer = correctAnswer.toLowerCase();
+  resDiv = document.getElementById("result_or_correct_answer");
+  if (userInput!==correctAnswer) {
+    resDiv.innerHTML="INCORRECT";
+  } else {
+    resDiv.innerHTML="CORRECT";
+  }
+}
+function showCorrect(){
+  resDiv = document.getElementById("result_or_correct_answer");
+  correctAnswer = correctAnswer.toLowerCase();
+  resDiv.innerHTML= correctAnswer;
 }
 nextCard();
