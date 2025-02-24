0c39fc4d33acd27511b3a0126107d95af355977c
Anton Gladyshev <returnstrike@yandex.com>
2025-02-24 15:45:43 +0200

    croco 2.0 done

---
+++ cards_crocodile.js
@@ -0,0 +1,16 @@
+cards = [
+  {"gr": "η αγάπη",
+   "en": "the love",
+   "level": 0,
+   "constructions": ["present"],
+   "gr_ex": [
+     {"present":["η αγάπη μου"]},
+   ],
+   "en_ex": [
+     {"present":["My love"]},
+   ],
+   "ctable": [
+   ],
+   "img":"decks/crocodile/love.jpg"},
+ ]
+nextCard();
