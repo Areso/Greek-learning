c2e4a0fa94bc8619f5f8b805d8580490089d7782
Anton Gladyshev <returnstrike@yandex.com>
2025-05-26 13:29:46 +0300

    added random words

---
+++ random_to_learn.js
@@ -0,0 +1,192 @@
+cards = [
+  {
+    "gr": "μην ανησυχείς",
+    "en": "don't worry",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["μην ανησυχείς"]}
+    ],
+    "en_ex": [
+      {"example":["don't worry"]}
+    ],
+    "img": "decks/dont_worry.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "δύο γυναίκες μαλώνουν",
+    "en": "two women argue",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["δύο γυναίκες μαλώνουν"]}
+    ],
+    "en_ex": [
+      {"example":["two women argue"]}
+    ],
+    "img": "decks/dont_worry.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "δύο γυναίκες μαλώνουν",
+    "en": "two women argue",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["δύο γυναίκες μαλώνουν"]}
+    ],
+    "en_ex": [
+      {"example":["two women argue"]}
+    ],
+    "img": "decks/dont_worry.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "είμαι αγχωμένος",
+    "en": "I am stressed",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["είμαι αγχωμένος"]}
+    ],
+    "en_ex": [
+      {"example":["I am stressed"]}
+    ],
+    "img": "decks/i_am_stressed.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "είμαι αγχωμένος",
+    "en": "I am stressed",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["είμαι αγχωμένος"]}
+    ],
+    "en_ex": [
+      {"example":["I am stressed"]}
+    ],
+    "img": "decks/i_am_stressed.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "μην φοβάσαι",
+    "en": "don't fear",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["μην φοβάσαι"]}
+    ],
+    "en_ex": [
+      {"example":["don't fear"]}
+    ],
+    "img": "decks/dont_fear.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "χαλάρωσε",
+    "en": "relax",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["χαλάρωσε"]}
+    ],
+    "en_ex": [
+      {"example":["relax"]}
+    ],
+    "img": "decks/relax.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "χαλάρωσε",
+    "en": "relax",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["χαλάρωσε"]}
+    ],
+    "en_ex": [
+      {"example":["relax"]}
+    ],
+    "img": "decks/relax.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "είναι φυσιολογικό να αγχώνεσαι",
+    "en": "to_be_stressed",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["είναι φυσιολογικό να αγχώνεσαι"]}
+    ],
+    "en_ex": [
+      {"example":["to_be_stressed"]}
+    ],
+    "img": "decks/to_be_stressed.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "Νομίζω ότι δεν είμαι έτοιμος για την εκδήλωση",
+    "en": "I think I am not ready for the event",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["Νομίζω ότι δεν είμαι έτοιμος για την εκδήλωση"]}
+    ],
+    "en_ex": [
+      {"example":["I think I am not ready for the event"]}
+    ],
+    "img": "decks/for_the_event.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "μίλα/μιλήστε αργάσε",
+    "en": "you speak slowly!",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["χαλάρωσε"]}
+    ],
+    "en_ex": [
+      {"example":["relax"]}
+    ],
+    "img": "decks/speak.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "Χαμογέλα",
+    "en": "Smile!",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["Χαμογέλα"]}
+    ],
+    "en_ex": [
+      {"example":["smile"]}
+    ],
+    "img": "decks/smile.jpg",
+    "labels": [""]
+  },
+  {
+    "gr": "Χαμογέλα",
+    "en": "Smile!",
+    "constructions": ["example"],
+    "gr_ex": [
+      {"example":["Χαμογέλα"]}
+    ],
+    "en_ex": [
+      {"example":["smile"]}
+    ],
+    "img": "decks/smile.jpg",
+    "labels": [""]
+  },
+]
+
+
+//χρειάζεσαι να ξεκουραστείς
+//όλα θα πάνε καλά
+
+//καλή επιτυχία
+
+//Νομίζω ότι δεν είμαι έτοιμος για την εκδήλωση
+//Μίλα/Μιλήστε αργά - speak slowly
+//Χαμογέλα!
+//Μπορείς να το κάνεις
+//Σκέφτομαι να πάρω καινούριο
+//Καλύτερα να πάρεις μεταχειρισμένο
+
+const script = document.createElement('script');
+script.src = 'to_learn.js';
+script.onload = function () {
+  // Optional: Code to run after file1.js is loaded
+  console.log('learn.js loaded!');
+};
+document.head.appendChild(script);
